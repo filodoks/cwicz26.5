35df926d7253d31a7e465a54467263443ff842cc
filodoks <filodoks@gmail.com>
2019-08-29 12:04:14 +0200

    Initial commit

---
+++ src/cokolwiek/company/Main.java
@@ -0,0 +1,11 @@
+package cokolwiek.company;
+
+public class Main implements NowyInterface {
+
+    public static void main(String[] args) {
+	Main main = new Main();
+        System.out.println(main.POLE_TYPU_INT);
+        System.out.println(main.POLE_TYPU_DOUBLE);
+
+    }
+}
---
+++ src/cokolwiek/company/NowyInterface.java
@@ -0,0 +1,6 @@
+package cokolwiek.company;
+
+public interface NowyInterface {
+    int POLE_TYPU_INT = 10;
+    double POLE_TYPU_DOUBLE = 1.2;
+}
